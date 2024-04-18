14396ea75fda5433d5087ed1f1f019262554e9bf
Yeeke2 <133200583+Yeeke2@users.noreply.github.com>
2024-04-18 11:13:19 +0200

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+ini_set('memory_limit', '255');
+
+echo ini_get("memory_limit");
---
+++ src/App/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_type=1);
+
+include __DIR__ . "/../Framework/App.php";
+
+use Framework\App;
+
+$app = new App();
+
+return $app;
